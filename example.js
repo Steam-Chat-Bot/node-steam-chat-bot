88f714f676baf34945e90243e5af98f8263da6c3
Efreak <Efreak2004@gmail.com>
2013-08-08 23:35:18 -0700

    add new functions setGames and setPrimaryGame. joinGame is kept for compatibility

--- example.js
+++ example.js
@@ -5,7 +5,6 @@ var ChatBot = require('steam-chat-bot').ChatBot;
 var myBot = new ChatBot('username', 'password', {
 //	sentryFile: '',		//Bot tries to find a sentry file automatically. This is only required if you have one with a strange name, otherwise it's automatic.
 //	guardCode: '',		//guardCode will override a sentry file. Comment this out after the first use.
-//	games: [440],		//this is a comma-separated array of games that the bot will play automatically on login. 440 is tf2.
 	autoReconnect: true	//automatically reconnect to the server
 	});
 
@@ -234,3 +233,9 @@ myBot.connect();
 var details = myBot.getTriggerDetails();
 myBot.clearTriggers();
 myBot.addTriggers(details);
+
+//these are useful for displaying your bot as playing a game, so it shows up in the upper part of the userlist.
+//this is a comma-separated array of games that the bot will play automatically on login. 440 is tf2.
+//myBot.setGames([440]);
+//this will stop all games, start the game listed (the first parameter), then after a delay in ms (the second param), start any games it was already playing. 570 is dota2.
+//myBot.setPrimaryGame(570,250);
