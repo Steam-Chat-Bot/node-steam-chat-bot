cad2f5028f5b06a43ad33f296d5ea2e5f3ad2e5c
Efreak <Efreak2004@gmail.com>
2014-11-01 21:31:28 -0700

    Switch infobot over to using sqlite instead of stupid file-based storage. Also supports sql and etc if you're running on heroku or some such

---
+++ import-infobot.js
@@ -0,0 +1,36 @@
+var storageDir = ''; //set this to the localStorage directory used by your bot
+var dbFile = ''; //set this to the db you wish to use for your new bot. You may want to create it first by simply running the bot with the new module.
+
+
+
+
+
+
+
+
+
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+}
+var ueberDB = require('ueberDB');
+var ubr = new ueberDB.database("sqlite", {filename:dbFile||'db.db'}, {writeInterval:1000});
+var fs = require('fs');
+var output = {};
+var db = new LocalStorage(storageDir||'.');
+var keys = db.keys;
+console.log("Starting import");
+process.stdout.write("Exporting: ");
+ubr.init(function(err){
+	if(err) {
+		console.log(err);
+		process.exit();
+	} else for (var key in keys) {
+		process.stdout.write(keys[key]+"        ");
+		var factoid = JSON.parse(db.getItem(keys[key]));
+		factoid.definition = [factoid.definition];
+		ubr.set(keys[key],factoid);
+		db.removeItem(keys[key]);
+	}
+	ubr.close(function(){console.log("\nDone!"); process.exit()});
+});
