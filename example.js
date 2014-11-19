62c19c99fc3919e0bd2c9e780f9264a80acb6905
Efreak <Efreak2004@gmail.com>
2014-11-18 23:48:02 -0800

    Standardize quotes

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
-var ChatBot = require('steam-chat-bot').ChatBot;
+var ChatBot = require("steam-chat-bot").ChatBot;
 
 // This will log in a steam user with the specified username and password 
 // You can also pass in a steam guard code from an email
-var myBot = new ChatBot('username', 'password', {
-//	sentryFile: '',		//Bot tries to find a sentry file automatically. This is only required if you have one with a strange name, otherwise it's automatic.
-//	guardCode: '',		//guardCode will override a sentry file. Comment this out after the first use.
+var myBot = new ChatBot("username", "password", {
+//	sentryFile: "",		//Bot tries to find a sentry file automatically. This is only required if you have one with a strange name, otherwise it's automatic.
+//	guardCode: "",		//guardCode will override a sentry file. Comment this out after the first use.
 	logFile: true,		//set to true to log to bot.$username.log, or define a custom logfile. Set to false if you don't want to log to file.
 	autoReconnect: true	//automatically reconnect to the server
 	});
@@ -14,19 +14,19 @@ myBot.addTriggers([
 
 	// Commands to stop/unstop the bot from saying anything in a chatroom
 	{ 
-		name: 'MuteCommand', 
-		type: 'BotCommandTrigger', 
+		name: "MuteCommand", 
+		type: "BotCommandTrigger", 
 		options: { 
-			matches: ['!mute', '!pause'], 
+			matches: ["!mute", "!pause"], 
 			exact: true,
 			callback: function(bot) { bot.mute(); }
 		} 
 	},
 	{ 
-		name: 'UnmuteCommand', 
-		type: 'BotCommandTrigger', 
+		name: "UnmuteCommand", 
+		type: "BotCommandTrigger", 
 		options: { 
-			matches: ['!unmute', '!unpause'], 
+			matches: ["!unmute", "!unpause"], 
 			exact: true,
 			callback: function(bot) { bot.unmute(); }
 		} 
@@ -34,10 +34,10 @@ myBot.addTriggers([
 
 	// Command to join Bad Rats whenever it's mentioned	
 	{ 
-		name: 'BadRatsCommand', 
-		type: 'BotCommandTrigger', 
+		name: "BadRatsCommand", 
+		type: "BotCommandTrigger", 
 		options: { 
-			matches: ['bat rats'], 
+			matches: ["bat rats"], 
 			exact: false,
 			callback: function(bot) { bot.joinGame(34900); }
 		} 
@@ -45,25 +45,25 @@ myBot.addTriggers([
 
 	// Automatically accept invites from any user to the specified group chat
 	{ 
-		name: 'AcceptChatInvite', 
-		type: 'AcceptChatInviteTrigger', 
+		name: "AcceptChatInvite", 
+		type: "AcceptChatInviteTrigger", 
 		options: { 
-			chatrooms: { 'GroupSteamId': 'Welcome message' }, 
+			chatrooms: { "GroupSteamId": "Welcome message" }, 
 			autoJoinAfterDisconnect: true
 		} 
 	},
 
 	// Automatically accept all friend requests
-	{ name: 'AcceptFriendRequest', type: 'AcceptFriendRequestTrigger' },
+	{ name: "AcceptFriendRequest", type: "AcceptFriendRequestTrigger" },
 
 	// Reply triggers - respond to a chat/private message if it matches a set of inputs 
 	// (case-insensitive exact or substring match), and choose randomly from a set of responses
 	{ 
-		name: 'EmptyQuoteReply', 
-		type: 'ChatReplyTrigger', 
+		name: "EmptyQuoteReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['^'], 
-			responses: ['^'], 
+			matches: ["^"], 
+			responses: ["^"], 
 			exact: true, 
 			delay: 1000, 
 			probability: 0.2, 
@@ -71,59 +71,59 @@ myBot.addTriggers([
 		} 
 	},
 	{ 
-		name: 'HeartReply', 
-		type: 'ChatReplyTrigger', 
+		name: "HeartReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['<3'], 
-			responses: ['</3', '<3'], 
+			matches: ["<3"], 
+			responses: ["</3", "<3"], 
 			exact: true, 
 			delay: 500, 
 			probability: 0.5, 
 			timeout: 60*60*1000 } },
 	{ 
-		name: 'PingReply', 
-		type: 'ChatReplyTrigger', 
+		name: "PingReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['ping'], 
-			responses: ['pong'], 
+			matches: ["ping"], 
+			responses: ["pong"], 
 			exact: true, 
 			delay: 1000, 
 			probability: 1, 
 			timeout: 10*1000 } },
 /*	you probably don't have :D: or :medicon: emotes on your bot, so this is commented out.
 	{ 
-		name: 'HealReply', 
-		type: 'ChatReplyTrigger', 
+		name: "HealReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['heal','health','heal me',"i'm hurt","I'm hurt",'im hurt','Im hurt'], 
-			responses: [':medicon:'], 
+			matches: ["heal","health","heal me","i'm hurt","I'm hurt","im hurt","Im hurt"], 
+			responses: [":medicon:"], 
 			exact: true, 
 			delay: 1000, 
 			probability: 1, 
 			timeout: 10*1000 } },
-		name: 'GrinReply', 
-		type: 'ChatReplyTrigger', 
+		name: "GrinReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: [':D'], 
-			responses: [':D:'], 
+			matches: [":D"], 
+			responses: [":D:"], 
 			exact: true, 
 			delay: 500, 
 			probability: 0.5, 
 			timeout: 60*1000 } },
 */	{ 
-		name: 'SteveHoltReply', 
-		type: 'ChatReplyTrigger', 
+		name: "SteveHoltReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['steve holt', 'steve holt!'], 
-			responses: ['\\o/'], 
+			matches: ["steve holt", "steve holt!"], 
+			responses: ["\\o/"], 
 			exact: false, 
 			delay: 500, 
 			timeout: 10*1000 
 		} 
 	},
 	{
-		name: 'SteamIDCheck', 
-		type: 'SteamInfoTrigger', 
+		name: "SteamIDCheck", 
+		type: "SteamInfoTrigger", 
 		options: { 
 			command: "!steamrep", 
 			delay: 2000, 
@@ -132,39 +132,39 @@ myBot.addTriggers([
 	},
 	// Reply triggers that will only respond to a particular user
 	{ 
-		name: 'SingleUserReply', 
-		type: 'ChatReplyTrigger', 
+		name: "SingleUserReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
-			matches: ['hi bot'], 
-			responses: ['hi boss!'], 
+			matches: ["hi bot"], 
+			responses: ["hi boss!"], 
 			exact: true, 
-			users: ['76561197961244239'] 
+			users: ["76561197961244239"] 
 		} 
 	},
 
 	// Sample regex trigger, "mate" will be responded to with "mmaaaate", 
 	// "mmaaaate" will be responded to with "mmmaaaaaaate", etc
 	{ 
-		name: 'MateEscalation', 
-		type: 'RegexReplaceTrigger',
-		options: { match: /^(m+?)(a+?)te(s??)$/, response: '{0}m{1}aaate{2}', delay: 500} 
+		name: "MateEscalation", 
+		type: "RegexReplaceTrigger",
+		options: { match: /^(m+?)(a+?)te(s??)$/, response: "{0}m{1}aaate{2}", delay: 500} 
 	},
 
 	// Butt bot, replace a random word from someone's message with "butt" about once every 50 messages
 	{ 
-		name: 'ButtBot', 
-		type: 'ButtBotTrigger', 
-		options: { replacement: 'butt', probability: 0.02, delay: 1000 } 
+		name: "ButtBot", 
+		type: "ButtBotTrigger", 
+		options: { replacement: "butt", probability: 0.02, delay: 1000 } 
 	},
 
 	// Chat reply that doesn't need a particular message to trigger, just a random reply about 
 	// once every 100 messages (and no more than once an hour)
 	{ 
-		name: 'RandomReply', 
-		type: 'ChatReplyTrigger', 
+		name: "RandomReply", 
+		type: "ChatReplyTrigger", 
 		options: { 
 			matches: [], 
-			responses: ['ლ(ಠ益ಠლ)', 'щ(ﾟДﾟщ)', 'omg', '(ﾉಥ益ಥ)ﾉ', '¯\\_(ツ)_/¯'], 
+			responses: ["ლ(ಠ益ಠლ)", "щ(ﾟДﾟщ)", "omg", "(ﾉಥ益ಥ)ﾉ", "¯\\_(ツ)_/¯"], 
 			delay: 500, 
 			probability: 0.01, 
 			timeout: 60*60*1000 
@@ -173,24 +173,24 @@ myBot.addTriggers([
 
 	// Cleverbot reply that only happens when the word "cleverbot" is mentioned
 	{ 
-		name: 'DirectCleverbotReply', 
-		type: 'CleverbotTrigger', 
-		options: { keywords: ['cleverbot'] } 
+		name: "DirectCleverbotReply", 
+		type: "CleverbotTrigger", 
+		options: { keywords: ["cleverbot"] } 
 	},
 
 	// Random cleverbot reply that triggers randomly about once every 100 messages
 	{ 
-		name: 'RandomCleverbotReply', 
-		type: 'CleverbotTrigger', 
+		name: "RandomCleverbotReply", 
+		type: "CleverbotTrigger", 
 		options: { probability: 0.01, timeout: 30*60*1000 } 
 	},
 
 	// Say something when a user joins chat
 	{ 
-		name: 'SteveHoltEnter', 
-		type: 'MessageOnJoinTrigger', 
+		name: "SteveHoltEnter", 
+		type: "MessageOnJoinTrigger", 
 		options: { 
-			user: '76561197961244239', 
+			user: "76561197961244239", 
 			message: "STEVE HOLT! \\o/", 
 			probability: 0.5, 
 			delay: 1000, 
@@ -200,38 +200,38 @@ myBot.addTriggers([
 
 	// Query Wolfram Alpha when a message starts with !wolfram
 	{ 
-		name: 'WolframReply', 
-		type: 'WolframAlphaTrigger', 
-		options: { command: '!wolfram', appId: 'XXXXXX' } 
+		name: "WolframReply", 
+		type: "WolframAlphaTrigger", 
+		options: { command: "!wolfram", appId: "XXXXXX" } 
 	},
 
 	//Query Urban Dictionary using their *unofficial* api when a message starts with !urban
 	{
-		name: 'WolframReply',
-		type: 'UrbanDictionaryTrigger',
-		options: { command: '!urban' }
+		name: "WolframReply",
+		type: "UrbanDictionaryTrigger",
+		options: { command: "!urban" }
 	},
 
 	// Post all links from chat to tumblr, and also post things on command
 	{ 
-		name: 'TumblrTrigger', 
-		type: 'TumblrTrigger', 
+		name: "TumblrTrigger", 
+		type: "TumblrTrigger", 
 		options: { 
 			autoPost: true, 
 			autoPostContext: false, 
-			blogName: 'XXX', 
-			consumerKey: 'XXX', 
-			consumerSecret: 'XXX', 
-			token: 'XXX', 
-			tokenSecret: 'XXX' 
+			blogName: "XXX", 
+			consumerKey: "XXX", 
+			consumerSecret: "XXX", 
+			token: "XXX", 
+			tokenSecret: "XXX" 
 		}  
 	},
 
 	// Search YouTube and respond with the top result whenever someone types !yt <query>, rickroll about 1 every 100 times
 	{ 
-		name: 'Youtube', 
-		type: 'YoutubeTrigger', 
-		options: { command: '!yt', rickrollChance: 0.01 } 
+		name: "Youtube", 
+		type: "YoutubeTrigger", 
+		options: { command: "!yt", rickrollChance: 0.01 } 
 	},
 ]);
 
