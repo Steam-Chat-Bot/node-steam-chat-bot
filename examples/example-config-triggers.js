2c1c3cc96b7c84056097af5d0bb5e4680caa36bf
Divinux <Divinux@gmx.de>
2015-05-14 03:43:48 +0200

    Lets try again :v

--- examples/example-config-triggers.js
+++ examples/example-config-triggers.js
@@ -216,7 +216,34 @@ module.exports = [
 			tokenSecret: "XXX" 
 		}  
 	},
-
+	//look for posted links, then respond with page title
+	{
+		name: 'linkname',
+		type: 'LinkName',
+		options: {}
+	},
+	// Greet every enterin person by name
+	{ 
+		name: "unnamed enter", 
+		type: "DoormatTrigger", 
+		options: { 
+			
+			delay: 4000, 
+			probability: 1.0, 
+			timeout: 60*1000
+		} 
+	},
+	//Private message trigger text
+	{ 
+		name: "PmReply", 
+		type: "ChatPmTrigger", 
+		options: { 
+			matches: ["whisper"], 
+			responses: ["shh! secret!"], 
+			exact: true, 
+			delay: 1000, 
+			probability: 1, 
+			timeout: 10*1000 } },
 	// Search YouTube and respond with the top result whenever someone types !yt <query>, rickroll about 1 every 100 times
 	{ 
 		name: "Youtube", 
